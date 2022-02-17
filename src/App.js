72de8f9891810eb6e743d7f1379c6ed438ced58c
Tyler Hawkins <tylerh@adobe.com>
2022-02-17 10:24:21 -0700

    style: adds new styles to each section of demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Section } from './Section';
 import { TitleAttributeBad } from './01_title_attribute/TitleAttributeBad';
 import { TitleAttributeGood } from './01_title_attribute/TitleAttributeGood';
 import { ShortContentHoverFocusBad } from './02_short_content_hover_focus/ShortContentHoverFocusBad';
@@ -13,36 +14,47 @@ export const App = () => (
   <main>
     <h1>Accessible Tooltips</h1>
     <hr />
-    <h2>
-      Using the <code>title</code> Attribute
-    </h2>
-    <h3>Bad Example</h3>
-    <TitleAttributeBad />
-    <h3>Good Example</h3>
-    <TitleAttributeGood />
-    <hr />
-    <h2>Short Content (Hover/Focus)</h2>
-    <h3>Bad Example</h3>
-    <ShortContentHoverFocusBad />
-    <h3>Good Example</h3>
-    <ShortContentHoverFocusGood />
-    <hr />
-    <h2>Short Content (Click/Keypress)</h2>
-    <h3>Bad Example</h3>
-    <ShortContentClickKeypressBad />
-    <h3>Good Example</h3>
-    <ShortContentClickKeypressGood />
-    <hr />
-    <h2>Long Content</h2>
-    <h3>Bad Example</h3>
-    <LongContentBad />
-    <h3>Good Example</h3>
-    <LongContentGood />
-    <hr />
-    <h2>Interactive Content</h2>
-    <h3>Bad Example</h3>
-    <p>TODO</p>
-    <h3>Good Example</h3>
-    <p>TODO</p>
+
+    <Section>
+      <h2>
+        Using the <code>title</code> Attribute
+      </h2>
+      <h3>Bad Example</h3>
+      <TitleAttributeBad />
+      <h3>Good Example</h3>
+      <TitleAttributeGood />
+    </Section>
+
+    <Section>
+      <h2>Short Content (Hover/Focus)</h2>
+      <h3>Bad Example</h3>
+      <ShortContentHoverFocusBad />
+      <h3>Good Example</h3>
+      <ShortContentHoverFocusGood />
+    </Section>
+
+    <Section>
+      <h2>Short Content (Click/Keypress)</h2>
+      <h3>Bad Example</h3>
+      <ShortContentClickKeypressBad />
+      <h3>Good Example</h3>
+      <ShortContentClickKeypressGood />
+    </Section>
+
+    <Section>
+      <h2>Long Content</h2>
+      <h3>Bad Example</h3>
+      <LongContentBad />
+      <h3>Good Example</h3>
+      <LongContentGood />
+    </Section>
+
+    <Section>
+      <h2>Interactive Content</h2>
+      <h3>Bad Example</h3>
+      <p>TODO</p>
+      <h3>Good Example</h3>
+      <p>TODO</p>
+    </Section>
   </main>
 );
---
+++ src/Section.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Section.css';
+
+export const Section = ({ children }) => (
+  <section className="section">{children}</section>
+);
